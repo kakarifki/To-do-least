abbb4d5f52387823b1d98db6c2b758b61617246c
M Rifki Septiawan <rifkim91@gmail.com>
2024-11-10 11:53:22 +0700

    fix: delete error lines on app tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Carausell from './components/carausell'
 
 
 function App() {
-    const [count, setCount] = useState(0)
+    // const [count, setCount] = useState(0)
 
     return (
         <>
