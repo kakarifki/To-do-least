2c21fd0011465217aaba4557ac57fafdd6f001da
M Rifki Septiawan <rifkim91@gmail.com>
2024-11-18 19:14:01 +0700

    learn: modify main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './App.tsx'
+import { TaskProvider } from './components/task-context.tsx';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App />
+    <TaskProvider>
+      <App />
+    </TaskProvider>
     </StrictMode>
 )
