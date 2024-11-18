c82c149764ddd4f5699e6967c47ba0fbefddfdd6
M Rifki Septiawan <rifkim91@gmail.com>
2024-11-18 19:23:54 +0700

    learn: Show new task with priority

--- src/components/item-task/index.tsx
+++ src/components/item-task/index.tsx
@@ -1,20 +1,26 @@
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
+import { useTaskContext } from "../../components/task-context";
 
 interface ItemTaskProps {
-    id: string;
-    taskName: string;
+  id: string;
+  taskName: string;
+  priority: 'Urgent' | 'Normal' | 'Casual';
 }
 
-const ItemTask = ({ id, taskName }: ItemTaskProps) =>  {
-    return (
-        <li className='flex justify-around items-center p-4 bg-gray-100 rounded-lg shadow hover:bg-gray-200 transition' id={id}>
-            {taskName}
-            <Checkbox />
-            <Button variant="secondary" className='hover:bg-blue-500'>Edit Task</Button>
-            <Button variant="destructive" className='hover:bg-red-600'>Delete Task</Button>
-        </li>
-    );
+const ItemTask = ({ id, taskName, priority }: ItemTaskProps) => {
+  const { deleteTask } = useTaskContext();
+
+  return (
+    <li className='flex justify-between items-center p-4 bg-gray-100 rounded-lg shadow hover:bg-gray-200 transition' id={id}>
+      <span>{taskName} ({priority})</span>
+      <Checkbox />
+      <Button variant="secondary" className='hover:bg-blue-500'>Edit Task</Button>
+      <Button onClick={() => deleteTask(id)} variant="destructive" className='hover:bg-red-600'>
+        Delete Task
+      </Button>
+    </li>
+  );
 };
 
 export default ItemTask;
--- src/components/list-tasks/index.tsx
+++ src/components/list-tasks/index.tsx
@@ -1,26 +1,19 @@
 import ItemTask from "../item-task";
-
-const listTodo = [
-  { id: '1', nameTodo: 'Task 1' },
-  { id: '2', nameTodo: 'Task 2' },
-  { id: '3', nameTodo: 'Task 3' },
-  { id: '4', nameTodo: 'Task 4' },
-  { id: '5', nameTodo: 'Task 5' },
-];
+import { useTaskContext } from "../../components/task-context";
 
 const ListTask = () => {
-  return (
-    <>
-    <div className='container mx-auto mt-4'>
-        <h2 className='flex justify-center text-xl font-semibold text-gray-700'>Task List</h2>
-        <ul className='flex flex-col gap-4 mt-4'>
-        {listTodo.map(task => (
-                    <ItemTask key={task.id} id={task.id} taskName={task.nameTodo} />
-                ))}
-        </ul>
-      </div>
-    </>
-  )
-}
+  const { tasks } = useTaskContext();
 
-export default ListTask
+  return (
+    <div className='container mx-auto mt-4'>
+      <h2 className='flex justify-center text-xl font-semibold text-gray-700'>Task List</h2>
+      <ul className='flex flex-col gap-4 mt-4'>
+        {tasks.map(task => (
+          <ItemTask key={task.id} id={task.id} taskName={task.nameTodo} priority={task.priority} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ListTask;
