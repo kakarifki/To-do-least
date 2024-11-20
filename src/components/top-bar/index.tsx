d52ce893cbc1f4b6f07a42184aa6890702f9e503
M Rifki Septiawan <rifkim91@gmail.com>
2024-11-20 17:25:55 +0700

    feat: Redesign top bar

--- src/components/top-bar/index.tsx
+++ src/components/top-bar/index.tsx
@@ -1,13 +1,16 @@
+import { Button } from '@/components/ui/button';
 
 const TopBar = () => {
   return (
-    <>
-<div className='container mx-auto px-4 flex flex-col bg-blue-50 p-4 rounded shadow-md'>
-        <h1 className='mx-auto text-2xl font-bold text-blue-700'>To-do-list</h1>
-        
+    <div className="container mx-auto px-4 flex flex-col bg-blue-50 p-4 rounded shadow-md">
+      <h1 className="mx-auto text-2xl font-bold text-blue-700">To-Do-Least</h1>
+      <div className="flex justify-end mt-4">
+        <Button variant="outline" className="bg-blue-600 text-white">
+          Add New Task
+        </Button>
       </div>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default TopBar
+export default TopBar;
