beffe97f25a6c7e5fd216b1d93f4c58ea3bd06e3
M Rifki Septiawan <rifkim91@gmail.com>
2024-11-20 19:27:14 +0700

    learn: trying react-hook-form error handling

--- src/components/new-task/index.tsx
+++ src/components/new-task/index.tsx
@@ -1,4 +1,5 @@
 import { useForm } from 'react-hook-form';
+import { ErrorMessage } from "@hookform/error-message"
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 
@@ -14,7 +15,7 @@ interface NewTaskProps {
 }
 
 const NewTask = ({ addTask }: NewTaskProps) => {
-  const { register, handleSubmit, reset } = useForm<TaskForm>();
+  const { register, formState: { errors }, handleSubmit, reset } = useForm<TaskForm>();
 
   const onSubmit = (data: TaskForm) => {
     addTask(data.taskName, data.details, data.dueDate, data.category);
@@ -25,8 +26,28 @@ const NewTask = ({ addTask }: NewTaskProps) => {
     <div className="container mx-auto p-4">
       <h2 className="text-lg font-semibold mb-2">Add New Task</h2>
       <form className="flex flex-col gap-2" onSubmit={handleSubmit(onSubmit)}>
-        <Input placeholder="Task Name" {...register('taskName')} />
-        <Input placeholder="Task Details" {...register('details')} />
+        <Input placeholder="Task Name" {...register('taskName', {required: 'Harus diisi oy'})} />
+        <ErrorMessage
+        errors={errors}
+        name="taskName"
+        render={({ message }) => <p className='bg-red-400'>{message}</p>}
+      />
+        <Input placeholder="Task Details" {...register('details', {
+          required: "This is required.",
+          maxLength: {
+            value: 10,
+            message: "This input exceed maxLength.",
+          },})} />
+          <ErrorMessage
+        errors={errors}
+        name="details"
+        render={({ messages }) =>
+          messages &&
+          Object.entries(messages).map(([type, message]) => (
+            <p key={type}>{message}</p>
+          ))
+        }
+      />
         <Input type="date" {...register('dueDate')} />
         <select className="border rounded p-2" {...register('category')}>
           <option value="Work">Work</option>
