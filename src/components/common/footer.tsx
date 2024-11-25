fdac657031c53a50da1d13da3d08fce57fd03622
M Rifki Septiawan <rifkim91@gmail.com>
2024-11-25 19:58:40 +0700

    feat: add footer

---
+++ src/components/common/footer.tsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+  return (
+    <div className="bg-blue-500 text-white text-center py-4">
+    <h1 className="text-l font-bold">This is Footer. Copiright 2024 KakaRifki</h1>
+    </div>
+  )
+}
+
+export default Footer
--- src/components/common/layout/index.tsx
+++ src/components/common/layout/index.tsx
@@ -1,5 +1,6 @@
 import { FC, ReactNode } from 'react';
 import Navbar from '@/components/common/navbar';
+import Footer from '../footer';
 
 interface LayoutProps {
   children: ReactNode;
@@ -10,6 +11,7 @@ const Layout: FC<LayoutProps> = ({ children }) => {
     <div className="min-h-screen">
       <Navbar />
       {children}
+      <Footer />
     </div>
   );
 };
